4235399c55ea8aee54d2ce5d19f6f2b04e3bdedb
camilleandrade125 <camilleandrade125@gmail.com>
2023-12-06 10:25:30 -0300

    Melhorando p design do projeto

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-const api = "http://localhost:3001/pessoas/"
+const api = "https://pokeapi.co/api/v2/pokemon/";
+
+function aoclicar() {
+    const pesquisaInput = document.getElementById("pesquisa-poke");
+    const pokeImagem = document.getElementById("imagem-poke");
+    const pokeNome = document.getElementById("nome-poke");
 
 
-function aoclicar(){
 
 
-
-    const pesquisaInput = document.getElementById("pesquisa-poke")
-    const pokeImagem = document.getElementById("imagem-poke")
-    const pokeNome = document.getElementById("nome-poke")
-
-    let valorAtualInput = pesquisaInput.value
-    let pesquisaAtualPoke = api+valorAtualInput
-    fetch(pesquisaAtualPoke).then(function(resultado){
-        return resultado.json()
-
-    }).then(function(pessoa){
-        
+    let valorAtualInput = pesquisaInput.value;
+    let pesquisaAtualPoke = api + valorAtualInput;
     
+    fetch(pesquisaAtualPoke)
+        .then(function (resultado) {
+            return resultado.json();
+        })
+        .then(function (pokemon) {
+            pokeImagem.style.display = "block";
+            pokeNome.textContent = pokemon.name.toUpperCase();
+            pokeImagem.setAttribute("src", pokemon.sprites.front_default);
         
-        pokeNome.textContent = `Nome: ${pokemon.name}`;
-        pokeImagem.setAttribute("src", pokemon.sprites.front_default)
-
-
-    }).catch(function(erro){
-
-        alert(`Pokemon não existente ${valorAtualInput}`)
-    })
-
+        })
+        .catch(function (erro) {
+            alert(`Pokemon não existente ${valorAtualInput}`);
+        });
 }
-
-
