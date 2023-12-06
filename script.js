709e77304d47e97657f8b924fed6fd77ce88ef1f
camilleandrade125 <camilleandrade125@gmail.com>
2023-12-06 10:51:31 -0300

    Modificando estrutura asssincrona

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 const api = "https://pokeapi.co/api/v2/pokemon/";
 
-function aoclicar() {
+async function aoclicar() {
+
     const pesquisaInput = document.getElementById("pesquisa-poke");
     const pokeImagem = document.getElementById("imagem-poke");
     const pokeNome = document.getElementById("nome-poke");
 
 
-
-
     let valorAtualInput = pesquisaInput.value;
     let pesquisaAtualPoke = api + valorAtualInput;
+
+    try{
+
+        const resposta = await fetch(pesquisaAtualPoke)
+        const pokemon = await resposta.json();
+        pokeImagem.style.display = "block";
+        pokeNome.textContent = pokemon.name.toUpperCase();
+        pokeImagem.setAttribute("src", pokemon.sprites.front_default);
+
+    }catch{
+
+        alert(`Pokemon não existente ${valorAtualInput}`);
+
+    }
+
+
     
-    fetch(pesquisaAtualPoke)
-        .then(function (resultado) {
-            return resultado.json();
-        })
-        .then(function (pokemon) {
-            pokeImagem.style.display = "block";
-            pokeNome.textContent = pokemon.name.toUpperCase();
-            pokeImagem.setAttribute("src", pokemon.sprites.front_default);
-        
-        })
-        .catch(function (erro) {
-            alert(`Pokemon não existente ${valorAtualInput}`);
-        });
 }
+
